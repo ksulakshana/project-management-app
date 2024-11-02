76c77593f785b2338a01477d938ce037fff1ec90
ksulakshana <sulakshana146@gmail.com>
2024-11-02 19:31:42 +0530

    update 1st nov1

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(cors({origin: true}));
-
+// app.use(cors({origin: true}));
+app.use(cors());
 // app.use(incomingRequestLogger);
 app.use('/api/v1/user',userRoutes);
 app.use('/api/v1/user/task',taskRoutes);
